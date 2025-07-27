4c90020a80ce5b7e1e715bcf727dccaf0b86fcc9
dtorannpu <dtorannpu@gmail.com>
2025-07-27 11:58:14 +0900

    Add Testcontainers integration with PostgreSQL in test configuration

---
+++ src/test/java/com/example/axon/sample/TestConfig.java
@@ -0,0 +1,15 @@
+package com.example.axon.sample;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestConfig {
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgreSQLContainer() {
+    return new PostgreSQLContainer<>("postgres:latest");
+  }
+}
