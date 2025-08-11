d075af8bda99a5b6ab6c338fa7648504e75da789
dtorannpu <dtorannpu@gmail.com>
2025-08-11 12:55:45 +0900

    Render dynamic data in `index.html` with updates to `SampleController`
    - Add Thymeleaf `th` namespace to `index.html` and bind table content dynamically.
    - Modify `SampleController` to integrate `SampleQueryService` for fetching and mapping data.

--- src/main/java/com/example/controller/sample/SampleController.java
+++ src/main/java/com/example/controller/sample/SampleController.java
@@ -1,14 +1,24 @@
 package com.example.controller.sample;
 
+import com.example.query.service.SampleQueryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/sample")
 public class SampleController {
+  private final SampleQueryService sampleQueryService;
+
   @GetMapping
-  public String index() {
+  public String index(Model model) {
+    List<GetResponse> data =
+        sampleQueryService.findAll().stream().map(s -> new GetResponse(s.id(), s.body())).toList();
+    model.addAttribute("data", data);
     return "sample/index";
   }
 }
